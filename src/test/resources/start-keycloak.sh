e114229a0fca676fc21b50d4643685350d820ffb
Frank <frank@example.com>
2025-12-08 13:56:26 +0100

    scripts aangepast

--- src/test/resources/start-keycloak.sh
+++ src/test/resources/start-keycloak.sh
@@ -1,2 +1,4 @@
-# sudo mkdir -m 777 ./keycloak_data
+if ! test -d keycloak_data; then
+    sudo mkdir -m 777 ./keycloak_data
+fi
 podman run -p 8080:8080 -v ./keycloak_data:/opt/keycloak/data/h2 -e KEYCLOAK_ADMIN=admin -e KEYCLOAK_ADMIN_PASSWORD=admin quay.io/keycloak/keycloak:26.4.7 start-dev
