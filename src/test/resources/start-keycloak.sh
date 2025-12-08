d3d5bf710919bc827d465b9136b5d29d82ff02d5
Frank <frank@example.com>
2025-12-08 18:54:19 +0100

    clean up

--- src/test/resources/start-keycloak.sh
+++ src/test/resources/start-keycloak.sh
@@ -1,4 +1,14 @@
-if ! test -d keycloak_data; then
-    sudo mkdir -m 777 ./keycloak_data
+#!/bin/bash
+
+KEYCLOAK_DEV_DATA_DIR=./keycloak_data
+if ! test -d "$KEYCLOAK_DEV_DATA_DIR"; then
+    sudo mkdir -m 777 "$KEYCLOAK_DEV_DATA_DIR"
 fi
-podman run -p 8080:8080 -v ./keycloak_data:/opt/keycloak/data/h2 -e KEYCLOAK_ADMIN=admin -e KEYCLOAK_ADMIN_PASSWORD=admin quay.io/keycloak/keycloak:26.4.7 start-dev
+
+podman run \
+  --publish 8080:8080 \
+  --volume "$KEYCLOAK_DEV_DATA_DIR":/opt/keycloak/data/h2 \
+  -e KEYCLOAK_ADMIN=admin \
+  -e KEYCLOAK_ADMIN_PASSWORD=admin \
+  quay.io/keycloak/keycloak:26.4.7 \
+  start-dev
